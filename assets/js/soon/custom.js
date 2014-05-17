b8a317e41e36f7582ac42fa1ac4724ba44055267
cjmiska <claudestockton@gmail.com>
2014-05-17 22:25:18 +0100

    added svg hover

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -1,46 +1,80 @@
 /******************************************************************************************************************************
 COMMING SOON PAGE
 *******************************************************************************************************************************/
-(function($) {
+
+/*
+ * Replace all SVG images with inline SVG
+ * (Thanks to Drew Baker - http://bit.ly/1fcLElK)
+ */
+jQuery('img.svg').each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function (data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if (typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if (typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass + ' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
+
+(function ($) {
     /**
-    * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
-    * according to the GMT+0 Timezone
-    **/
+     * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
+     * according to the GMT+0 Timezone
+     **/
     var launch = new Date(2015, 06, 14, 11, 00);
     /**
-    * The script
-    **/
+     * The script
+     **/
     var message = $('#message');
     var days = $('#days');
     var hours = $('#hours');
     var minutes = $('#minutes');
     var seconds = $('#seconds');
-    
+
     setDate();
-    function setDate(){
+
+    function setDate() {
         var now = new Date();
-        if( launch < now ){
+        if (launch < now) {
             days.html('<h1>0</H1><p>Day</p>');
             hours.html('<h1>0</h1><p>Hour</p>');
             minutes.html('<h1>0</h1><p>Minute</p>');
             seconds.html('<h1>0</h1><p>Second</p>');
             message.html('OUR SITE IS NOT READY YET...');
-        }
-        else{
-            var s = -now.getTimezoneOffset()*60 + (launch.getTime() - now.getTime())/1000;
-            var d = Math.floor(s/86400);
-            days.html('<h1>'+d+'</h1><p>Day'+(d>1?'s':''),'</p>');
-            s -= d*86400;
+        } else {
+            var s = -now.getTimezoneOffset() * 60 + (launch.getTime() - now.getTime()) / 1000;
+            var d = Math.floor(s / 86400);
+            days.html('<h1>' + d + '</h1><p>Day' + (d > 1 ? 's' : ''), '</p>');
+            s -= d * 86400;
 
-            var h = Math.floor(s/3600);
-            hours.html('<h1>'+h+'</h1><p>Hour'+(h>1?'s':''),'</p>');
-            s -= h*3600;
+            var h = Math.floor(s / 3600);
+            hours.html('<h1>' + h + '</h1><p>Hour' + (h > 1 ? 's' : ''), '</p>');
+            s -= h * 3600;
 
-            var m = Math.floor(s/60);
-            minutes.html('<h1>'+m+'</h1><p>Minute'+(m>1?'s':''),'</p>');
+            var m = Math.floor(s / 60);
+            minutes.html('<h1>' + m + '</h1><p>Minute' + (m > 1 ? 's' : ''), '</p>');
 
-            s = Math.floor(s-m*60);
-            seconds.html('<h1>'+s+'</h1><p>Second'+(s>1?'s':''),'</p>');
+            s = Math.floor(s - m * 60);
+            seconds.html('<h1>' + s + '</h1><p>Second' + (s > 1 ? 's' : ''), '</p>');
             setTimeout(setDate, 1000);
 
             message.html('OUR SITE IS NOT READY YET, BUT WE ARE COMING SOON');
@@ -50,28 +84,29 @@ COMMING SOON PAGE
 /******************************************************************************************************************************
 ANIMATIONS
 *******************************************************************************************************************************/
-(function($) {
+(function ($) {
     "use strict";
     var isMobile = false;
-    if (navigator.userAgent.match(/Android/i) || 
+    if (navigator.userAgent.match(/Android/i) ||
         navigator.userAgent.match(/webOS/i) ||
-        navigator.userAgent.match(/iPhone/i) || 
-        navigator.userAgent.match(/iPad/i)|| 
-        navigator.userAgent.match(/iPod/i) || 
-        navigator.userAgent.match(/BlackBerry/i)) {                 
-        isMobile = true;            
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i)) {
+        isMobile = true;
     }
     if (isMobile == true) {
         $('body').removeClass('nomobile');
         $('.animated').removeClass('animated');
     }
-    $(function() {
+    $(function () {
         if (isMobile == false) {
             $('*[data-animated]').addClass('animated');
         }
+
         function animated_contents() {
             $(".animated:appeared").each(function (i) {
-                var $this    = $(this),
+                var $this = $(this),
                     animated = $(this).data('animated');
 
                 setTimeout(function () {
@@ -88,47 +123,46 @@ ANIMATIONS
 /******************************************************************************************************************************
 SLIDER
 *******************************************************************************************************************************/
-(function($) {
+(function ($) {
     "use strict";
-    $("body.nomobile #slider").revolution(
-    {
-            delay:9000,
-            startheight:450,
-            startwidth:890,
+    $("body.nomobile #slider").revolution({
+        delay: 9000,
+        startheight: 450,
+        startwidth: 890,
 
-            thumbWidth:100,
-            thumbHeight:50,
-            thumbAmount:5,
+        thumbWidth: 100,
+        thumbHeight: 50,
+        thumbAmount: 5,
 
-            onHoverStop:"on",
-            hideThumbs:200,
-            navigationType:"bullet",
-            navigationStyle:"round",
-            navigationArrows:"none",
+        onHoverStop: "on",
+        hideThumbs: 200,
+        navigationType: "bullet",
+        navigationStyle: "round",
+        navigationArrows: "none",
 
-            touchenabled:"on",
+        touchenabled: "on",
 
-            navOffsetHorizontal:0,
-            navOffsetVertical:80,
-            shadow:undefined,
-            fullWidth:"on",
-            fullScreen:"on"
+        navOffsetHorizontal: 0,
+        navOffsetVertical: 80,
+        shadow: undefined,
+        fullWidth: "on",
+        fullScreen: "on"
     });
 })(jQuery);
 /******************************************************************************************************************************
 BOOTSTRAP
 *******************************************************************************************************************************/
-(function($) {
+(function ($) {
     "use strict";
-        $('[data-rel=tooltip]').tooltip();
-        $(".alert").alert();
+    $('[data-rel=tooltip]').tooltip();
+    $(".alert").alert();
 })(jQuery);
 /******************************************************************************************************************************
 PROGRESS BAR
 *******************************************************************************************************************************/
-(function($) {
+(function ($) {
     "use strict";
-    $("a.btn-progress").click(function(){
-        $('#bar-container').slideToggle(); 
+    $("a.btn-progress").click(function () {
+        $('#bar-container').slideToggle();
     });
 })(jQuery);
