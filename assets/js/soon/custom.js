dc680aaf3aad1d5bb80dc591834d6110d21a204a
cjmiska <claudestockton@gmail.com>
2014-05-19 06:51:08 +0100

    removed text effect

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -35,22 +35,6 @@ jQuery('img.svg').each(function () {
 
 });
 
-function changecolor() {
-    var elem = document.getElementById("wrap");
-    var newAns = "";
-    for (var a = 0, l = elem.innerHTML.length; a < l; a++) {
-        newAns += '<span onmouseover="change1(this)" onmouseout="change2(this)" >' + elem.innerHTML.charAt(a) + '</span>';
-    }
-    elem.innerHTML = newAns;
-
-    function change1(x) {
-        x.style.color = "yellow";
-    }
-
-    function change2(x) {
-        x.style.color = "black";
-    }
-}
 
 (function ($) {
     /**
