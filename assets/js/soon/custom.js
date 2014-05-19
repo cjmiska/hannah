ab304d897df63b631d79c61dd5929cb3aaac4ff4
cjmiska <claudestockton@gmail.com>
2014-05-19 06:47:48 +0100

    added title effect

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -35,6 +35,23 @@ jQuery('img.svg').each(function () {
 
 });
 
+function changecolor() {
+    var elem = document.getElementById("wrap");
+    var newAns = "";
+    for (var a = 0, l = elem.innerHTML.length; a < l; a++) {
+        newAns += '<span onmouseover="change1(this)" onmouseout="change2(this)" >' + elem.innerHTML.charAt(a) + '</span>';
+    }
+    elem.innerHTML = newAns;
+
+    function change1(x) {
+        x.style.color = "yellow";
+    }
+
+    function change2(x) {
+        x.style.color = "black";
+    }
+}
+
 (function ($) {
     /**
      * Set your date here  (YEAR, MONTH (0 for January/11 for December), DAY, HOUR, MINUTE, SECOND)
